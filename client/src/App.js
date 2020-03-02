e559cd4c35dfa670701b3800d2228b0c313c4fa7
JangSungSik <sjwow77@naver.com>
2020-03-02 17:18:19 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -3,64 +3,83 @@ import Customer from './components/Customer';
 import './App.css';
 import Paper from '@material-ui/core/Paper';//컴포넌트 외부를 감싼다
 import Table from '@material-ui/core/Table';
+import TableRow from '@material-ui/core/TableRow';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 
 const styles = theme =>({
   root:{
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto'
+    marginTop: theme.spacing(3),
+    overflowX: "auto"
   },
   table:{
     minWidth: 1080
+  },
+  progress: {
+    margin : theme.spacing(2)
   }
 })
-const customers = [{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name' : '장승식',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name' : '홍길동',
-  'birthday': '441315',
-  'gender': '남자',
-  'job': '디자이너'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name' : '남정우',
-  'birthday': '643521',
-  'gender': '남자',
-  'job': '퍼블리셔'
-}]
+
+
+/* 컴포넌트 실행 순서
+1) constructor()
+2) componentWillMount()
+3) render()
+4) componentDidMount()
+*/
+
+/*
+ props or state => shoudComponentUpdate()
+ */
 
 class App extends Component {
+  //변경될수 있는 데이터 state
+  state = {
+    customers: "",
+    completed: 0
+  }
+
+  //모든 컴포넌트가 준비완료
+  componentDidMount(){
+    this.timer = setInterval(this.progress,20);
+    this.callApi()
+      .then(res => {
+        this.setState({customers:res})})
+      .catch(err => console.log(err));
+  }
+  
+  callApi = async()=>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({completed: completed >= 100 ? 0 : completed+1});
+  }
   render(){
     const { classes } = this.props;
     return (
       <Paper className={classes.root}>
         <Table className={classes.table}>
           <TableHead>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
           </TableHead>
           <TableBody>
-        { customers.map(c =>{
+        { this.state.customers ? this.state.customers.map(c =>{
             return( 
             <Customer
               key={c.id}
@@ -72,10 +91,15 @@ class App extends Component {
               job={c.job}
               />
             );
-          })}
+          }): 
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className="classes.progress" variant="determinate" value={this.state.completed}/>
+              </TableCell>
+            </TableRow>
+          }
           </TableBody>
         </Table>
-        
       </Paper>
     );
   }
--- server.js
+++ server.js
@@ -6,8 +6,34 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req,res) =>{
-   res.send({message:'Hello Express!'});
+app.get('/api/customers', (req, res) =>{
+  setTimeout(function(){
+    res.send([{
+      'id': 1,
+      'image': 'https://placeimg.com/64/64/1',
+      'name' : '장승식',
+      'birthday': '961222',
+      'gender': '남자',
+      'job': '대학생'
+    },
+    {
+      'id': 2,
+      'image': 'https://placeimg.com/64/64/2',
+      'name' : '홍길동',
+      'birthday': '441315',
+      'gender': '남자',
+      'job': '디자이너'
+    },
+    {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/3',
+      'name' : '남정우',
+      'birthday': '643521',
+      'gender': '남자',
+      'job': '퍼블리셔'
+    }])
+  },1000)
+   
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
